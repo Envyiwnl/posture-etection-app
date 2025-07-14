99f9583628408659609102f456d6ef0f8e6affd4
Envyiwnl <94548758+Envyiwnl@users.noreply.github.com>
2025-07-14 14:10:52 +0530

    Updated Server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import postureRoutes from './routes/postureRoutes.js';
 import fs from 'fs';
 
 const app = express ();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 
 //Middleware used
